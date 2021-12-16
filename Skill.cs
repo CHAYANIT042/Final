c4377ae4b125dde345c79bdc5833426bd12dfbc2
CHAYANIT042 <78426311+CHAYANIT042@users.noreply.github.com>
2021-12-16 11:29:10 +0700

    Add files via upload

---
+++ Skill.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Console_20211216
+{
+    public class Skill
+    {
+        public string SkillName { get; set; }
+
+        public int LvSkill { get; set; }
+    }
+}
